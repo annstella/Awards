6dfb0c27f04bdb4630c670576f314fcd5b166715
Annstella <annstellawangui@gmail.com>
2018-10-12 15:00:42 +0300

    created trmplate file

--- task/views.py
+++ task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+
 
 # Create your views here.
+def welcome(request):
+   return render(request, 'all-task/index.html')
+
+def task(request):
+   task = Image.objects.all()
+
+   return render(request, 'all-task/index.html', {"task":task})
