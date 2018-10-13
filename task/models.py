4f84b59bf2e455a32e83e32d8865f9bf2f5fd76f
Annstella <annstellawangui@gmail.com>
2018-10-13 12:34:03 +0300

    created profile class

--- task/models.py
+++ task/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=50, default= ' new bio ' )
+    profile_pic = models.ImageField(upload_to='profile/' , default = 'default.jpg' )
