46e6c8ea3db7d82b2051524dba14b1684c3c50b1
Annstella <annstellawangui@gmail.com>
2018-10-13 14:10:44 +0300

    added search method in the views

--- task/views.py
+++ task/views.py
@@ -9,3 +9,16 @@ def welcome(request):
 #    task = Image.objects.all()
 
 #    return render(request, 'all-task/index.html', {"task":task})
+
+def search_results(request):
+  
+    if 'projects' in request.GET and request.GET["projects"]:
+        category = request.GET.get("projects")
+        searched_categories = Image.search_image(category)
+        message = f"{category}"
+
+        return render(request, 'all-task/search.html',{"message":message,"projects": searched_categories})
+
+    else:
+        message = " Found 0 images for the search term"
+        return render(request, 'all-task/search.html',{"message":message})
