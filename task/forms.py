c1eb7138a7a602372002d6d45dbf41b8128565b1
Annstella <annstellawangui@gmail.com>
2018-10-13 19:47:38 +0300

    added profile and post form

--- task/forms.py
+++ task/forms.py
@@ -2,3 +2,17 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Projects
+
+class NewsLetterForm(forms.Form):
+    your_name = forms.CharField(label='First Name',max_length=30)
+    email = forms.EmailField(label='Email')
+
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ['user', 'post']
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Projects
+        exclude = ['creator', 'likes', 'time',  'profile']
