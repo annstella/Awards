a35c49f3c5a391ac5dc1272a2c9b6da59f02fead
Annstella <annstellawangui@gmail.com>
2018-10-13 18:49:00 +0300

    created login template

--- task/views.py
+++ task/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
+@login_required(login_url='/login')
 def welcome(request):
     return render(request,'indexx.html')
 
@@ -14,15 +15,15 @@ def welcome(request):
 
 def search_results(request):
   
-    if 'projects' in request.GET and request.GET["projects"]:
+    if 'username' in request.GET and request.GET["projects"]:
         category = request.GET.get("projects")
-        searched_categories = Projects.search_image(category)
+        searched_categories = Projects.search_project(category)
         message = f"{category}"
 
         return render(request, 'all-task/search.html',{"message":message,"projects": searched_categories})
 
     else:
-        message = " Found 0 images for the search term"
+        message = " Found 0 projects for the search term"
         return render(request, 'all-task/search.html',{"message":message})
 
 
@@ -41,3 +42,22 @@ def task_today(request):
     else:
         form = NewsLetterForm()
     return render(request, 'indexx.html', {"projects":projects,"letterForm":form})
+
+@login_required(login_url='/accounts/login/')
+def profile(request, user_username = None):
+    if user_username == None:
+        user = request.user
+    else:
+        user = get_object_or_404( User ,username = user_username)
+    
+    profile = Profile.get_user_profile( user )
+    title = "Profile"
+    projects = Projects.get_projects_by_id(user.id)
+    print(projects)
+
+    context = {
+        'title':title, 
+        "projects":projects,
+        "profile" : profile }
+        
+    return render(request, 'profile/profile.html',context)
