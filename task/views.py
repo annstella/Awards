2219d74bf45573162ad8d3d05a894dbe77605472
Annstella <annstellawangui@gmail.com>
2018-10-13 15:21:41 +0300

    installed django registration

--- task/views.py
+++ task/views.py
@@ -15,7 +15,7 @@ def search_results(request):
   
     if 'projects' in request.GET and request.GET["projects"]:
         category = request.GET.get("projects")
-        searched_categories = Image.search_image(category)
+        searched_categories = Projects.search_image(category)
         message = f"{category}"
 
         return render(request, 'all-task/search.html',{"message":message,"projects": searched_categories})
