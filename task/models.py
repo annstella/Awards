5f7e06586aad15c4d6c61a78a6c530eb2c4adf18
Annstella <annstellawangui@gmail.com>
2018-10-13 19:35:17 +0300

    added tinymice to our models

--- task/models.py
+++ task/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime as dt
-# from tinymce.models import HTMLField
+from tinymce.models import HTMLField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
